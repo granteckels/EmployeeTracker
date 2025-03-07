fcb0f7d8a008f5548909ef26e790604620fc99f3
granteckels <geckels1@gmail.com>
2025-03-06 19:43:36 -0600

    Actually finish ViewAllEmployees Previous commit actually finished ViewAllDepartments

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const client = new Client({
 })
 await client.connect();
 
+async function ViewAllEmployees() {
+    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title as title, department.name as department, role.salary as salary, manager.first_name || ' ' || manager.last_name as manager FROM employee\n"
+        + "JOIN role ON role_id = role.id\n"
+        + "JOIN department ON role.department = department.id\n"
+        + "LEFT JOIN employee manager ON employee.manager_id = manager.id";
+
+    const result = await client.query(query);
+
+    const table = new cliTable({
+        head: ['id', 'first_name', 'last_name', 'title', 'department', 'salary', 'manager'],
+        colWidths: [5, 20, 20, 40, 15, 10, 40],
+    });
+    result.rows.forEach(row => {
+        table.push([row.id, row.first_name, row.last_name, row.title, row.department, row.salary, row.manager]);
+    });
+
+    console.log(table.toString());
+}
+
 async function ViewAllRoles() {
     const query = "SELECT role.id, title, salary, department.name as department FROM role\n"
         + "JOIN department ON department = department.id";
@@ -19,7 +38,7 @@ async function ViewAllRoles() {
 
     const table = new cliTable({
         head: ['id', 'title', 'salary', 'department'],
-        colWidths: [5, 25, 10, 15],
+        colWidths: [5, 40, 10, 15],
     });
     result.rows.forEach(row => {
         table.push([row.id, row.title, row.salary, row.department]);
@@ -72,7 +91,7 @@ class Cli {
                 // console.log(answer)
                 switch (answer.action) {
                     case 'View All Employees':
-                        console.log('VAE');
+                        ViewAllEmployees().then(() => this.startCli());
                         break;
                     case 'Add Employee':
                         console.log('AE');
